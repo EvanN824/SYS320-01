53b324f8971d79a242d7cd70815f04142a9f146a
Evan Norzow <evan.norzow@mymail.champlain.edu>
2024-09-12 16:05:58 -0400

    lab2 complete

---
+++ week3/Script1.ps1
@@ -0,0 +1,65 @@
+﻿function PrintPreviousLogs($days){
+
+$loginouts = Get-EventLog System -Source Microsoft-Windows-WinLogOn -After (Get-Date).AddDays($days)
+
+$logTable = @()
+for($i = 0; $i -lt $loginouts.Count; $i++)
+{
+    $events = ""
+    if ($loginouts[$i].InstanceId -eq 7001) {$events = "LogOn"}
+    if ($loginouts[$i].InstanceId -eq 7002) {$events = "LogOff"}
+
+    $user = $loginouts[$i].ReplacementStrings[1]
+
+    #So this is how you make objects? Cool.
+    $userTranslate = New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList $user
+    $user = $userTranslate.Translate([System.Security.Principal.NTAccount]).Value
+
+
+    $logTable += [pscustomobject]@{`
+    "Time" = $loginouts[$i].TimeGenerated;`
+    "Id" = $loginouts[$i].InstanceId;`
+    "Event" = $events;`
+    "User" = $user;
+    }
+                  
+}
+
+return $logTable 
+}
+
+function PrintShutDownLogs($days){
+
+$turnoffs = Get-EventLog System -After (Get-Date).AddDays($days) | Where-Object {$_.EventID -eq 6006}
+$turnons = Get-EventLog System -After (Get-Date).AddDays($days) | Where-Object {$_.EventID -eq 6005}
+
+$logTable = @()
+for($i = 0; $i -lt $turnoffs.Count; $i++)
+{
+    $events = "LogOn"
+
+    $user = "System"
+
+
+    $logTable += [pscustomobject]@{`
+    "Time" = $turnoffs[$i].TimeGenerated;`
+    "Id" = $turnoffs[$i].InstanceId;`
+    "Event" = $events;`
+    "User" = $user;
+    }
+
+    $events = "LogOff"
+
+    $logTable += [pscustomobject]@{`
+    "Time" = $turnons[$i].TimeGenerated;`
+    "Id" = $turnons[$i].InstanceId;`
+    "Event" = $events;`
+    "User" = $user;
+    }
+                  
+}
+
+
+
+return $logTable 
+}
---
+++ week3/Script2.ps1
@@ -0,0 +1,9 @@
+﻿. (Join-Path $PSScriptRoot Script1.ps1)
+
+clear
+
+$toPrint = PrintShutDownLogs -14
+$toPrint
+
+$toWrite = PrintPreviousLogs -14
+$toWrite
