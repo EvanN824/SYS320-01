e3d38600bbb1712e5fbf69e8378e43894a14f887
Evan Norzow <evan.norzow@mymail.champlain.edu>
2024-09-26 15:33:15 -0400

    week 4

---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,16 @@
+﻿function GetAddresses ($page, $httpcode, $browser){
+
+$notfounds = Get-Content C:\xampp\apache\logs\access.log | select-string -Pattern $page | select-string -Pattern $httpcode | Select-String -Pattern $browser
+    
+$regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+$ipsUnorganized = $regex.Matches($notfounds)
+
+$ips = @()
+for ($i = 0; $i -lt $ipsUnorganized.Count; $i++)
+{
+    $ips += [pscustomobject]@{"IP" = $ipsUnorganized[$i].Value;}
+}
+$ipsoftens = $ips | Where-Object {$_.IP -ilike "10.*"}
+return $ipsoftens
+}
---
+++ week4/main.ps1
@@ -0,0 +1,9 @@
+﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+$page = 'index.html'
+$http = ' 404 '
+$browser = 'Chrome'
+
+$ipsoftens = GetAddresses $page $http $browser
+$counts = $ipsoftens | Group-Object -Property Count
+$counts | Select-Object Count, Name
