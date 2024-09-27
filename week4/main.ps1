6ea2009d93ddc3fa34bab49becdba9a1a3bd349b
Evan Norzow <evan.norzow@mymail.champlain.edu>
2024-09-26 22:11:37 -0400

    parsing apache logs complete

--- week4/main.ps1
+++ week4/main.ps1
@@ -1,4 +1,5 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Apache-CustomObject.ps1)
 
 $page = 'index.html'
 $http = ' 404 '
@@ -7,3 +8,6 @@ $browser = 'Chrome'
 $ipsoftens = GetAddresses $page $http $browser
 $counts = $ipsoftens | Group-Object -Property Count
 $counts | Select-Object Count, Name
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
