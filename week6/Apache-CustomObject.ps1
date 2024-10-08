540db2aba8a8c20097eeb4dac4cbdddb75faee36
Evan Norzow <evan.norzow@mymail.champlain.edu>
2024-10-08 17:25:20 -0400

    added TurnToMenu

---
+++ week6/Apache-CustomObject.ps1
@@ -0,0 +1,24 @@
+﻿function ApacheLogs1(){
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i = 0; $i -lt $logsNotformatted.Count; $i++){
+
+$words = $logsNotformatted[$i].Split(" ");
+
+$tableRecords += [pscustomobject]@{ 
+"IP" = $words[0];
+"Time" = $words[3].Trim('[');
+"Method" = $words[4].Trim('"');
+"Page" = $words[6];
+"Protocol" = $words[7];
+"Response" = $words[8];
+"Referrer" = $words[10];
+"Client" = $words[11..($words.Count - 1)];
+}
+
+}
+
+return $tableRecords | Where-Object {$_.IP -like "10.*"}
+
+}
---
+++ week6/TurnToMenu.ps1
@@ -0,0 +1,65 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot Apache-CustomObject.ps1)
+
+clear
+
+$Prompt  = "Please choose your operation:`n"
+$Prompt += "1 - Display Apache Logs`n"
+$Prompt += "2 - Display last failed logins`n"
+$Prompt += "3 - Display At-Risk Users`n"
+$Prompt += "4 - Open Champlain Site`n"
+$Prompt += "5 - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $logs = ApacheLogs1
+        $toPrint = $logs[-10..-1]
+
+        $toPrint
+    }
+
+    elseif($choice -eq 2){
+        $failedLogins = getFailedLogins 90
+        $toPrint = $failedLogins[-10..-1]
+
+        Write-Host ($toPrint | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 3){ 
+
+        $riskyUsers = getAtRiskUsers 90
+        Write-Host ($riskyUsers | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 4){
+
+        $chrome = Get-Process -Name chrome
+        if($chrome)
+        {} else
+        {
+        Start-Process "title" "C:\Program Files\Google\Chrome\Application\chrome.exe" --start-fullscreen "https://www.champlain.edu/"
+        }
+    }
+
+    else{
+        Write-Host "Invalid option, please try again."
+    }
+}
