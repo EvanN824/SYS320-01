d523ae319ff627863022203b47c419518cf698c7
Evan Norzow <evan.norzow@mymail.champlain.edu>
2024-10-08 15:52:01 -0400

    First week 6 assignment

--- week5/CallScript.ps1
+++ week5/CallScript.ps1
@@ -1,4 +1,9 @@
 ﻿. (Join-Path $PSScriptRoot ClassScanner.ps1)
 
 $printable = gatherClasses
-$printable
+$ITSInstructors = $printable | Where-Object {($_."Class Code" -clike "SYS*") -or `
+($_."Class Code" -clike "NET*") -or ($_."Class Code" -clike "SEC*") -or ($_."Class Code" -clike "FOR*") -or `
+($_."Class Code" -clike "CSI*") -or ($_."Class Code" -clike "DAT*") } | Sort-Object "Instructor" `
+| Select-Object "Instructor" -Unique
+
+$printable | foreach {$_.Instructor -in $ITSInstructors.Instructor} | Select-Object "Instructor" | Group-Object Count,Name | Sort-Object Count -Descending                                          
--- week5/ClassScanner.ps1
+++ week5/ClassScanner.ps1
@@ -2,11 +2,12 @@
 
 for($i=0; $i -lt $FullTable.length; $i++){
 $Days = @()
-if($FullTable[$i].Days -ilike "M"){$Days += "Monday"}
-if($FullTable[$i].Days -ilike "*T[WF]*"){$Days += "Tuesday"}
-if($FullTable[$i].Days -ilike "W"){$Days += "Wednesday"}
-if($FullTable[$i].Days -ilike "TH"){$Days += "Thursday"}
-if($FullTable[$i].Days -ilike "F"){$Days += "Friday"}
+if($FullTable[$i].Days -ilike "M*"){$Days += "Monday"}
+if($FullTable[$i].Days -ilike "*T[TWF]*"){$Days += "Tuesday"}
+if($FullTable[$i].Days -like "T"){$Days += "Tuesday"}
+if($FullTable[$i].Days -ilike "*W*"){$Days += "Wednesday"}
+if($FullTable[$i].Days -ilike "*TH*"){$Days += "Thursday"}
+if($FullTable[$i].Days -ilike "*F*"){$Days += "Friday"}
 
 $FullTable[$i].Days = $Days
 
